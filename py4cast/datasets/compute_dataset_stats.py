a3d7120c11685dfa7c1ae7631a8a5b6dc8029bc4
FERREIRA mathilde <mathilde.ferreira@meteo.fr>
2025-05-16 16:14:07 +0200

    parralelisation of stat

--- py4cast/datasets/compute_dataset_stats.py
+++ py4cast/datasets/compute_dataset_stats.py
@@ -1,5 +1,9 @@
 from typing import Literal
 import warnings
+import gc
+from joblib import Parallel, delayed
+from base import Item
+
 
 import torch
 from tqdm import tqdm
@@ -7,6 +11,22 @@ from tqdm import tqdm
 from py4cast.datasets.base import DatasetABC
 from py4cast.utils import torch_save
 
+def compute_stats_worker(batch: Item):
+    tensor = getattr(batch, type_tensor).tensor
+    tensor = tensor.flatten(1, 3)  # Flatten to be (Batch, X, Features)
+    counter = tensor.shape[0]  # += batch size
+
+    sum_means = torch.nansum(tensor.nanmean(dim=1), dim=0)  # (d_features)
+    sum_squares = torch.nansum((tensor**2).nanmean(dim=1), dim=0)  # (d_features)
+
+    # pevent etre calculer en remplacant nan soit par +inf soit -inf
+    # torch.nan_to_num(a, -torch.inf)
+    mini = torch.min(torch.nan_to_num(tensor, torch.inf), 1).values[0]
+
+    # pevent etre calculer en remplacant nan soit par +inf soit -inf
+    # torch.nan_to_num(a, -torch.inf)
+    maxi = torch.max(torch.nan_to_num(tensor, -torch.inf), 1).values[0]
+    return counter, sum_means, sum_squares, mini, maxi
 
 def compute_mean_std_min_max(
     dataset: DatasetABC, type_tensor: Literal["inputs", "outputs", "forcing"]
@@ -15,14 +35,26 @@ def compute_mean_std_min_max(
     Compute mean and standard deviation for this dataset.
     """
     random_batch = next(iter(dataset.torch_dataloader()))
+
+    # initialisation en dehors du worker
     named_tensor = getattr(random_batch, type_tensor)
+    # ne pas recalculer les stats deja calculé dans les inputs
+    if type_tensor == "output":
+        named_tensor_input = getattr(random_batch, "input")
+        commun_feature = set(named_tensor.feature_names) & set(named_tensor_input.feature_names)
+        idx_nocommun_feature = [i for i, feature_name in enumerate(named_tensor.feature_names) if feature_name not in commun_feature ]
+        named_tensor = NamedTensor(
+            named_tensor.tensor[:, :, :, idx_nocommun_feature],
+            named_tensor.names,
+            named_tensor.feature_names[idx_nocommun_feature],
+            named_tensor.feature_dim_name,
+        )
+
     n_features = len(named_tensor.feature_names)
     sum_means = torch.zeros(n_features)
     sum_squares = torch.zeros(n_features)
     ndim_features = len(named_tensor.tensor.shape) - 1
     flat_input = named_tensor.tensor.flatten(0, ndim_features - 1)  # (X, Features)
-    # pevent etre calculer en remplacant nan soit par +inf soit -inf
-    # torch.nan_to_num(a, -torch.inf)
     best_min = torch.min(torch.nan_to_num(flat_input, torch.inf), dim=0).values
     best_max = torch.max(torch.nan_to_num(flat_input, -torch.inf), dim=0).values
 
@@ -31,38 +63,39 @@ def compute_mean_std_min_max(
         warnings.warn(
             "Your dataset contain NaN values, statistics will be calculated with zeros instead of NaN."
         )
-
     counter = 0
+    
+
     if dataset.settings.standardize:
         raise ValueError("Your dataset should not be standardized.")
 
-    # à parralelliser 
-    for batch in tqdm(
-        dataset.torch_dataloader(),
-        desc=f"Computing {type_tensor} stats",
-    ):
-    # Donc la on reprends a chaque fois le meme dataloader pour charger inputs, outputs ou forcings.
-    # pas possible de tout faire en meme temps et de calculer pour les parametre plutot que pour les inputs, outputs, forcings
-    # si tous de la meme taille on concatene sur les features ou sinon on fait un flatten pour chaque en enlevant les parametre qui ont deja été calculé
-        tensor = getattr(batch, type_tensor).tensor
-        tensor = tensor.flatten(1, 3)  # Flatten to be (Batch, X, Features)
-        counter += tensor.shape[0]  # += batch size
+    # ---------------------------------------------------
 
-        sum_means += torch.nansum(tensor.nanmean(dim=1), dim=0)  # (d_features)
-        sum_squares += torch.nansum((tensor**2).nanmean(dim=1), dim=0)  # (d_features)
-
-        # pevent etre calculer en remplacant nan soit par +inf soit -inf
-        # torch.nan_to_num(a, -torch.inf)
-        mini = torch.min(torch.nan_to_num(tensor, torch.inf), 1).values[0]
+    results = list(
+     tqdm(
+                Parallel(return_as="generator", n_jobs=5)(
+                    delayed(compute_stats_worker)(batch)
+                    for  batch in dataset.torch_dataloader()
+                ),
+                total=len(dataset.torch_dataloader()),
+                desc=f"Computing {type_tensor} stats",
+            )
+        )
+     
+    for result in results:
+        counter_worker, mean, square, mini, maxi = result
+        counter += counter_worker
+        sum_means += mean
+        sum_squares += square
         stack_mini = torch.stack([best_min, mini], dim=0)
         best_min = torch.min(stack_mini, dim=0).values  # (d_features)
-
-        # pevent etre calculer en remplacant nan soit par +inf soit -inf
-        # torch.nan_to_num(a, -torch.inf)
-        maxi = torch.max(torch.nan_to_num(tensor, -torch.inf), 1).values[0]
         stack_maxi = torch.stack([best_max, maxi], dim=0)
         best_max = torch.max(stack_maxi, dim=0).values  # (d_features)
 
+        del mean; del square; del mini; del maxi
+        gc.collect()
+
+    # en dehors du workers
     # attention dans counter aux colonne de nan pour l'instant pas pris en compte
     mean = sum_means / counter
     second_moment = sum_squares / counter
@@ -86,12 +119,10 @@ def compute_parameters_stats(dataset: DatasetABC):
     """
     all_stats = {}
     # les param en input output calculé 2 fois wtf
+
     for type_tensor in ["inputs", "outputs", "forcing"]:
-        stats_dict = compute_mean_std_min_max(dataset, type_tensor)
-        for feature, stats in stats_dict.items():
-            # If feature was computed multiple times we keep only first occurence
-            if feature not in all_stats.keys():
-                all_stats[feature] = stats
+        stats_dict = compute_mean_std_min_max(dataset)
+        all_stats = stats_dict | all_stats
 
     dest_file = dataset.cache_dir / "parameters_stats1.pt"
     torch_save(all_stats, dest_file)
