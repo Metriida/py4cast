f46d1a2cd06a34d2aa8937f685ea6fea1657b012
FERREIRA mathilde <mathilde.ferreira@meteo.fr>
2025-11-17 18:02:14 +0100

    remove useless variable

--- py4cast/lightning.py
+++ py4cast/lightning.py
@@ -610,12 +610,7 @@ class AutoRegressiveLightning(LightningModule):
                     )
                 elif ds:
                     # update the coarse forcings with our netwwork output
-                    first_coarse_step = batch.forcing.select_tensor_dim(
-                        "timestep", -1
-                    ).clone()
                     last_coarse_step = batch.forcing.select_dim("timestep", i).tensor.clone()
-                    print("first", first_coarse_step)
-                    print("last", last_coarse_step)
                     if self.mask_on_nan:
                         last_coarse_step = torch.nan_to_num(last_coarse_step, nan=0)
                     # only add common features
